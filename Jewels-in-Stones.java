da848b793de4c0955e7e7523c3b82705d3b908d2
cgalo <cgalo13@gmail.com>
2018-11-20 15:14:01 -0500

    Update Jewels-in-Stones.java
    You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
    
    The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".

--- Jewels-in-Stones.java
+++ Jewels-in-Stones.java
@@ -1,1 +1,45 @@
-/**/
+/*
+* Author: Carlos Galo
+*
+*
+*/
+
+public class JewelsandStones {
+
+    public static void main (String[] args) {
+
+        //You can edit the strings for any other letters you want to find in a string
+        String J = new String("abc");
+        String S = new String("abbbcdddd") ;
+
+        int totJinS = numJewelsInStones ( J , S ) ;
+
+        System.out.println ( totJinS ) ;
+    }
+
+    private static int numJewelsInStones ( String J, String S ) {
+
+        char jewels [] = J.toCharArray () ; //Convert String J into char array for easier comparison on loop
+        char stones [] = S.toCharArray () ; //Convert String S into char array for easier comparison on loop
+
+        int numJInS = 0; //Keep track of repetitive char, start with 0 as that's the least you can have
+
+
+        for ( int i = 0; i < stones.length ; i++ ) {
+            
+            for (int j = 0; j < jewels.length ; j ++) {
+                
+                if ( stones [ i ] == jewels [ j ] )
+                    
+                    numJInS ++ ;
+                
+            }
+        }
+
+        return numJInS;
+
+
+    }
+
+
+}
